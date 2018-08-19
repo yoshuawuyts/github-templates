27c886cea2f7f6850e3512a3a0408ee2f83590f4
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-08-19 19:29:53 +0200

    write_all

--- src/templates.rs
+++ src/templates.rs
@@ -1,7 +1,11 @@
+use failure::ResultExt;
 use mkdirp::mkdirp;
+
+use std::fs::File;
+use std::io::prelude::*;
 use std::path::PathBuf;
 
-use failure::ResultExt;
+static BUG_REPORT: &str = include_str!("../templates/bug_report.md");
 
 /// GitHub template struct.
 pub struct Templates {
@@ -10,10 +14,24 @@ pub struct Templates {
 }
 
 impl Templates {
-  /// Create a new instance.
+  /// Create a new instance. Creates a `.github` directory if it doesn't exist
+  /// already.
   pub fn new(mut dir: PathBuf, name: String) -> ::Result<Self> {
     dir.push(".github");
-    mkdirp(&dir).context(::ErrorKind::Other);
+    mkdirp(&dir).context(::ErrorKind::Other)?;
     Ok(Self { name, dir })
+  }
+
+  pub fn write_all(&self) -> ::Result<()> {
+    let issue_dir = self.dir.join("ISSUE_TEMPLATE");
+    mkdirp(&issue_dir).context(::ErrorKind::Other)?;
+
+    let mut buf = File::create(issue_dir.join("bug_report.md"))
+      .context(::ErrorKind::Other)?;
+
+    buf
+      .write_all(BUG_REPORT.as_bytes())
+      .context(::ErrorKind::Other)?;
+    Ok(())
   }
 }
