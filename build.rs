5f7d451a27ff919e2855a4d0dda019439cf4f57e
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-08-21 21:20:52 +0200

    improve build.rs

--- build.rs
+++ build.rs
@@ -1,3 +1,4 @@
+use std::path::PathBuf;
 use std::str::FromStr;
 use structopt::{clap::Shell, StructOpt};
 
@@ -6,9 +7,15 @@ include!("src/lib.rs");
 fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
   let outdir = ::std::env::var_os("OUT_DIR").expect("OUT_DIR not found.");
   let mut app = cli::Cli::clap();
+
+  let target: PathBuf = outdir.into();
+  let target = target.join("../../../scripts");
+  mkdirp::mkdirp(&target)?;
+
   for shell in &Shell::variants() {
     let shell = Shell::from_str(*shell)?;
-    app.gen_completions(env!("CARGO_PKG_NAME"), shell, &outdir);
+    app.gen_completions(env!("CARGO_PKG_NAME"), shell, &target);
   }
+
   Ok(())
 }
